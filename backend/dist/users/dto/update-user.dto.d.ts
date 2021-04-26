bbe98c594b5d2d12e67329b619fdc69cd03de05e
torque ninja <torqueninja22@hotmail.com>
2021-04-27 00:17:10 +0800

    pushed gitignore for backend

--- backend/dist/users/dto/update-user.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class UpdateUserDto {
-    favoriteFoods: string[];
-    age: number;
-}
--- backend/dist/users/schemas/user.schema.d.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-export declare type UserDocument = User & Document;
-export declare class User {
-    userId: string;
-    email: string;
-    age: number;
-    favoriteFoods: string[];
-}
-export declare const UserSchema: import("mongoose").Schema<Document<User, {}>, import("mongoose").Model<any, any>, undefined>;
