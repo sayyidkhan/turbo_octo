dfdcbb5982bf773046c1b59f280ceb36a659ec7d
torque ninja <torqueninja22@hotmail.com>
2021-05-01 12:35:15 +0800

    fixed the boilerplate code which made front-end unable to connect to backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(function (req, res, next ) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
+    next();
+  });
   await app.listen(5000);
 }
 bootstrap();
