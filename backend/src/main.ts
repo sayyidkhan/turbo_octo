f1a86b4513c1b74a456e42c7dea5df694d4e909b
torque ninja <torqueninja22@hotmail.com>
2021-05-02 22:03:43 +0800

    fix cors on the project

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const cors = require("cors");
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(function (req, res, next ) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+
+  app.use(cors({ origin: true }));
+
   await app.listen(5000);
 }
 bootstrap();
