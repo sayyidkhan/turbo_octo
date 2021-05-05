9ee6abaea6e49e82c6172e99f1b5c6500aa5d724
joeynmq <37472597+joeynmq@users.noreply.github.com>
2021-05-05 22:32:39 +0800

    fix alerts form only visible by admin

--- front-end/src/pages/PublicAlerts.tsx
+++ front-end/src/pages/PublicAlerts.tsx
@@ -1,44 +1,56 @@
-import React from 'react';
 import '../App.css';
 import {IssueAlerts} from "../components/issuealerts/IssueAlertsComponent";
 import {AlertsTable} from '../components/alertstable/AlertsTableComponent';
-import {Box} from "@material-ui/core"
 import CurrentLoginUserComponent from '../components/CurrentLoginUserComponent';
 
 export default function Dashboard() {
-    return (
-        <div className="dashboard-container-general">
-            <h1>Public alerts</h1>
-            <div className="under-page-title-div">
-                <CurrentLoginUserComponent/>
-            </div>
-            <div className="dashboard-container">
     
-                {/* THIS IS FOR ADMIN */}
+    const userType = sessionStorage.getItem("userType");
 
-                <div className="dashboard-tableContainer-div">
-                    <h2>Issue new alert</h2>
-                    <div className="account-typical-content-div">
-                      <IssueAlerts />
-                    </div>
+    if(userType === "government"){
+        return (
+            <div className="dashboard-container-general">
+                <h1>Public alerts</h1>
+                <div className="under-page-title-div">
+                    <CurrentLoginUserComponent/>
                 </div>
-
-                <div className="dashboard-tableContainer-div">
-                    <h2>Public alerts issued</h2>
-                    <div className="account-typical-content-div">
-                      <AlertsTable />
+                <div className="dashboard-container">
+        
+                    <div className="dashboard-tableContainer-div">
+                        <h2>Issue new alert</h2>
+                        <div className="account-typical-content-div">
+                        <IssueAlerts />
+                        </div>
                     </div>
-                </div>
 
-                {/* THIS IS FOR BUSINESS */}
-
-                <div className="dashboard-tableContainer-div">
-                    <h2>Public alerts</h2>
-                    <div className="account-typical-content-div">
-                      <AlertsTable />
+                    <div className="dashboard-tableContainer-div">
+                        <h2>Public alerts</h2>
+                        <div className="account-typical-content-div">
+                        <AlertsTable />
+                        </div>
                     </div>
+                
                 </div>
             </div>
-        </div>
-    )
+        );
+    } else {
+        return (
+            <div className="dashboard-container-general">
+                <h1>Public alerts</h1>
+                <div className="under-page-title-div">
+                    <CurrentLoginUserComponent/>
+                </div>
+                <div className="dashboard-container">
+
+                    <div className="dashboard-tableContainer-div">
+                        <h2>Public alerts</h2>
+                        <div className="account-typical-content-div">
+                        <AlertsTable />
+                        </div>
+                    </div>
+                
+                </div>
+            </div>
+        );
+    }
 }
