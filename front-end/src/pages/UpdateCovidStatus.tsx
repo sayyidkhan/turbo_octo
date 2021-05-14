cf2a273b4b4646bf3f05aa45af693adc8ea5681b
shieldofchaos <37095549+shieldofchaos@users.noreply.github.com>
2021-05-15 00:11:01 +0800

    completed covid-19 status update page

--- front-end/src/pages/UpdateCovidStatus.tsx
+++ front-end/src/pages/UpdateCovidStatus.tsx
@@ -1,9 +1,22 @@
 import '../App.css';
+import {UpdateCovidStatus} from "../components/updatecovidstatus/UpdateCovidStatusComponent";
+import CurrentLoginUserComponent from '../components/CurrentLoginUserComponent';
 
-export default function UpdateCovidStatus() {
-
+export default function CovidStatus() {
     return (
-        <div></div>
+        <div className="dashboard-container-general">
+            <h1>Trigger alerts</h1>
+            <div className="under-page-title-div">
+                <CurrentLoginUserComponent/>
+            </div>
+            <div className="dashboard-container">
+                <div className="dashboard-tableContainer-div">
+                    <h2>Trigger alert to government</h2>
+                    <div className="account-typical-content-div">
+                        <UpdateCovidStatus />
+                    </div>
+                </div>
+            </div>
+        </div>
     );
-
 }
