a931d756aa5bbfa67a9c9f405020d5a303819747
joeynmq <37472597+joeynmq@users.noreply.github.com>
2021-05-18 01:36:18 +0800

    commit search vaccince cert feature

--- front-end/src/components/vacccert/VaccCertComponent.tsx
+++ front-end/src/components/vacccert/VaccCertComponent.tsx
@@ -1,8 +1,15 @@
-import React, {Component} from 'react';
+import {Component} from 'react';
 import './vaccCert.css';
 import octo from '../../img/octo.png'
 
-export class VaccCertComponent extends Component {
+interface IProps {
+    userData : any;
+}
+
+interface IState {
+}
+
+export class VaccCertComponent extends Component<IProps, IState> {
 
     state = {
         loadingStatus : true,
@@ -10,43 +17,26 @@ export class VaccCertComponent extends Component {
         result : []
     }
 
-    // componentDidMount() {
-    //     getUsers_API().then(res => {
-    //         const result = res;
-    //         console.log(result);
-    //         this.setState({'result' : res.data , 'status' : res.status });
-    //     }).catch(err => {
-    //         console.log(err);
-    //         this.setState({'result' : [] , 'status' : err.status })
-    //     });
-    // }
-
-    // createList() {
-    //     //if status is success and list is not empty
-    //     if(this.state.status === 200 && this.state.result.length !== 0){
-    //         const myList = this.state.result;
-    //         return <ScrollerComponent myList={myList} />;
-    //     }
-    //     else {
-    //         return <p>No record.</p>
-    //     }
-    // }
-
     render() {
+        console.log(this.props.userData);
+        const v = this.props.userData.v_cert;
+        const p = this.props.userData.p_nric;
+        const e = this.props.userData.e_nric;
+
         return (
             <div className='VaccCert-wrapper'>
                 <table>
                     <tr>
-                        <td>v_cert_id</td>
+                        <td>{v.v_cert_id}</td>
                         <td><h3>Certification of Vaccination</h3></td>
-                        <td>v_date</td>
+                        <td>{v.v_date}</td>
                     </tr>
                 </table>
                 <img src={octo} className="center" alt="Insert Octo pic"/>
                 <table>
                     <tr>
-                        <div className='left'><td>p_nric, p_firstName, p_lastName</td></div>
-                        <div className='right'><td>e_nric, e_firstName, e_lastName</td></div>
+                        <div className='left'><td>{p.p_nric}, {p.firstname} {p.lastname}</td></div>
+                        <div className='right'><td>{e.e_nric}, {e.firstname} {e.lastname}</td></div>
                         <td></td>
                     </tr>
                 </table>
