74baaa85030094c4063aea96d6350c98fcbfdc0d
joeynmq <37472597+joeynmq@users.noreply.github.com>
2021-05-18 01:58:59 +0800

    fixed bugs for vacc cert

--- front-end/src/components/vacccert/VaccCertComponent.tsx
+++ front-end/src/components/vacccert/VaccCertComponent.tsx
@@ -12,31 +12,53 @@ interface IState {
 export class VaccCertComponent extends Component<IProps, IState> {
 
     state = {
-        loadingStatus : true,
-        status : 0,
-        result : []
+        v_cert_id: '',
+        v_date: '',
+        p_nric: '',
+        p_firstname: '',
+        p_lastname: '',
+        e_nric: '',
+        e_firstname: '',
+        e_lastname: ''
+    }
+
+    componentDidUpdate() {
+        if(this.state.v_cert_id === ''){
+            this.setData(this.props.userData);
+        }
+    }
+
+    setData(userData : any){
+        this.setState({
+            v_cert_id: userData.v_cert.v_cert_id,
+            v_date: userData.v_cert.v_date,
+            p_nric: userData.p_nric.p_nric,
+            p_firstname: userData.p_nric.firstname,
+            p_lastname: userData.p_nric.lastname,
+            e_nric: userData.e_nric.e_nric,
+            e_firstname: userData.e_nric.firstname,
+            e_lastname: userData.e_nric.lastname
+        });
     }
 
     render() {
-        console.log(this.props.userData);
-        const v = this.props.userData.v_cert;
-        const p = this.props.userData.p_nric;
-        const e = this.props.userData.e_nric;
+
+        const {v_cert_id, v_date, p_nric, p_firstname, p_lastname, e_nric, e_firstname, e_lastname } = this.state;
 
         return (
             <div className='VaccCert-wrapper'>
                 <table>
                     <tr>
-                        <td>{v.v_cert_id}</td>
+                        <td>{v_cert_id}</td>
                         <td><h3>Certification of Vaccination</h3></td>
-                        <td>{v.v_date}</td>
+                        <td>{v_date}</td>
                     </tr>
                 </table>
                 <img src={octo} className="center" alt="Insert Octo pic"/>
                 <table>
                     <tr>
-                        <div className='left'><td>{p.p_nric}, {p.firstname} {p.lastname}</td></div>
-                        <div className='right'><td>{e.e_nric}, {e.firstname} {e.lastname}</td></div>
+                        <div className='left'><td>{p_nric}, {p_firstname} {p_lastname}</td></div>
+                        <div className='right'><td>{e_nric}, {e_firstname} {e_lastname}</td></div>
                         <td></td>
                     </tr>
                 </table>
