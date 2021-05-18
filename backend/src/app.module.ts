6c66691e9b18dd9f43ee916f11863347fd28fdcb
torque ninja <torqueninja22@hotmail.com>
2021-05-18 17:54:11 +0800

    remove deprecated api

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,7 +3,6 @@ import {MongooseModule} from '@nestjs/mongoose';
 
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
-import {UsersModule} from './users/users.module';
 import {LocationModule} from './location/location.module';
 import {E_UserModule} from "./e_user/e_User.module";
 import {AlertListModule} from "./alert_list/alertlist.module";
@@ -18,7 +17,6 @@ const DATABASE_NAME = "demo";
 @Module({
   imports: [
       MongooseModule.forRoot(`mongodb://localhost/${DATABASE_NAME}`, { useFindAndModify: false }),
-      UsersModule,
       LocationModule,
       E_UserModule,
       AlertListModule,
